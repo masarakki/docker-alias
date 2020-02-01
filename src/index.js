d539b9590d202716b6297c628dd8b717835d25a4
masarakki <masaki182@gmail.com>
2020-02-01 14:56:40 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { getInput } from '@actions/core';
 import { exec } from '@actions/exec';
 
 const setup = () => {
-  exec('docker pull vvakame/review');
+  const dockerImage = getInput('image');
+  exec(`docker pull ${dockerImage}`);
 };
 
 setup();
